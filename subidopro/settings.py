ea59343563c9e9b74b09e43d8fa753bb8d2889d8
unknown <gabriel@stalse.com>
2025-07-11 10:32:20 -0300

    refactor: update database settings for local and production environments

--- subidopro/settings.py
+++ subidopro/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
