9e8fc2d8ae360900dca7d89b0f766d94e9a3bcde
unknown <gabriel@stalse.com>
2025-08-13 09:08:13 -0300

    feat: Implement health check and logs pages; add new views and update URLs

--- teste_app/urls.py
+++ teste_app/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('aprovacao_envio_valor_fixo/', TestViewEnvioAproveFixo, name='aprovacao_envio_valor_fixo'),
-    path('health/', HealthSubidometroView, name='health_subidometro'),
+    path('aprovacao_envio_coproducao/', TestViewEnvioAproveCoproducao, name='aprovacao_envio_coproducao'),
+    path('health_subidometro/', HealthSubidometroView, name='health_subidometro'),
+    path('logs/', cloud_run_logs, name='logs'),
 ]
